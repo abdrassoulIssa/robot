916f904fa280f3aaa5f388284bdc9199c4e06c03
Issa ABDRASSOUL YOUSSOUF <abdrassoul.issa@gmail.com>
2015-04-22 16:23:32 +0200

    Important updates

---
+++ Robot/src/robot/cmd/ctrl/MyCOM.java
@@ -0,0 +1,37 @@
+package robot.cmd.ctrl;
+import java.util.Arrays;
+
+import jssc.SerialPort;
+import jssc.SerialPortList;
+ 
+public class MyCOM {
+ 
+   public static void main(String[] args) {
+      // get computer serial ports names
+      String[] portNames = SerialPortList.getPortNames();
+      for (String port : portNames) {
+         System.out.println(port);
+      }
+ 
+      System.out.println(Arrays.toString(portNames)+"here");
+      // initialization with selecting port for communication
+      SerialPort serialPort = new SerialPort("COM3");
+ 
+      /*
+      try {
+         // open port for communication
+         serialPort.openPort();
+         // baundRate, numberOfDataBits, numberOfStopBits, parity
+         serialPort.setParams(9600, 8, 1, 0);
+         // byte data transfer
+         serialPort.writeBytes("text to tranfer".getBytes());
+         // String data transfer
+         serialPort.writeString("text to tranfer");
+         // close port
+         serialPort.closePort();
+      } catch (SerialPortException ex) {
+         System.out.println(ex);
+      }
+      */
+   }
+}
